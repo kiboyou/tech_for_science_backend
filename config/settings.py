d5ec6c0594ea9d2cc3b0340fc1f589fba50310ea
Ouattara Kiboyou mohamed <86033140+kiboyou@users.noreply.github.com>
2025-09-21 22:07:33 +0100

    update 1

--- config/settings.py
+++ config/settings.py
@@ -32,8 +32,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool, default=False)
 
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv(), default='localhost,127.0.0.1')
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
